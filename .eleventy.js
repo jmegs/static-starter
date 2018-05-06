487b893630628b27cd5a3e7b80d59bad1620b978
John Meguerian <johnmeguerian@gmail.com>
2018-05-06 18:39:56 -0400

    add implicit figures markdown plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,12 @@
 module.exports = function(config) {
+  let markdownIt = require("markdown-it")
+  let figures = require("markdown-it-implicit-figures")
+  let options = {
+    typographer: true
+  }
+
+  config.setLibrary("md", markdownIt(options).use(figures))
+
   return {
     dir: {
       input: "site",
