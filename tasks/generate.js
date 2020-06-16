adfd487c2e04c9b4bdfc54d25774a8b4990888ec
John Meguerian <johnmeguerian@gmail.com>
2020-06-15 20:07:23 -0400

    Delete old files

--- tasks/generate.js
+++
@@ -1,8 +0,0 @@
-const exec = require("child_process").exec
-
-module.exports = function generate(done) {
-  const cmd = `NODE_ENV=${process.env.NODE_ENV} eleventy`
-  exec(cmd, err => {
-    done(err)
-  })
-}
