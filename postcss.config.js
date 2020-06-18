01227630f16e7bf497271bbd7caf6f0cadef34eb
John Meguerian <johnmeguerian@gmail.com>
2020-06-17 22:29:26 -0400

    New CSS Pipeline
    processes scss => postcss => css

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,5 @@ const easyImport = require("postcss-easy-import")
 const presetEnv = require("postcss-preset-env")
 
 module.exports = {
-  plugins: [easyImport, presetEnv()]
+  plugins: [easyImport, presetEnv({ features: { "nesting-rules": true } })]
 }
